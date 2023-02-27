4e94e71798f4e3e675871af9b8fb8959ee702574
gwcorbett143 <gcorbett23@kentdenver.org>
2023-02-27 09:59:41 -0700

    Listing class and HashTable method complete

---
+++ HashTables.java
@@ -0,0 +1,62 @@
+package Hashtables;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HashTables {
+
+    public class Listing{
+        private String key, value;
+
+        public Listing(String key, String value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public String getValue() {
+            return value;
+        }
+
+        public void setValue(String value) {
+            this.value = value;
+        }
+
+        public String getKey() {
+            return key;
+        }
+
+        public void setKey(String key) {
+            this.key = key;
+        }
+    }
+
+    List<Listing>[] table;
+    int capacity;
+
+    //initializes an array of size capacity
+    public void HashTable(int capacity){
+        this.capacity = capacity;
+        table = new ArrayList[capacity];
+        for(int i = 0; i < capacity; i++){
+            table[i] = new ArrayList<>();
+        }
+    }
+
+
+
+    //put hashes the key to an index in your array, and places the value there. Fails if there are collisions/repeat keys.
+    public boolean put(String key, String value){
+
+    }
+
+    //get hashes the key to get the index, and returns that element. Returns null if key not found.
+    public String get(String key){
+
+    }
+
+    //returns the unique int in the range of the [0, array length)
+    private int hashCode(String key){
+
+    }
+
+}
