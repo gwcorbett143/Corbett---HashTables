0206d0435512bfdac0ed5531a123950d0ede3f57
gwcorbett143 <gcorbett23@kentdenver.org>
2023-02-27 10:02:58 -0700

    put method complete

--- HashTables.java
+++ HashTables.java
@@ -46,7 +46,13 @@ public class HashTables {
 
     //put hashes the key to an index in your array, and places the value there. Fails if there are collisions/repeat keys.
     public boolean put(String key, String value){
-
+        for(Listing e : table[hashCode(key)]){
+            if(e.getKey().equals(key)){
+                return false;
+            }
+        }
+        table[hashCode(key)].add(new Listing(key, value));
+        return true;
     }
 
     //get hashes the key to get the index, and returns that element. Returns null if key not found.
